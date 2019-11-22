1517410799d2e40a0b83a30bdbadefa0fd44d894
ademata <aldemata1@gmail.com>
2019-11-22 09:30:18 +0800

    Updates

---
+++ precache-manifest.81337c8671d17faaba63c316710a5967.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ebae62317f614c9b709ca14e058f2c9",
+    "url": "/robotfriends/index.html"
+  },
+  {
+    "revision": "ab3297cac8b5c3818335",
+    "url": "/robotfriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "dd9bbdbde8645bbaba78",
+    "url": "/robotfriends/static/css/main.ea4c3886.chunk.css"
+  },
+  {
+    "revision": "ab3297cac8b5c3818335",
+    "url": "/robotfriends/static/js/2.daf3ba0b.chunk.js"
+  },
+  {
+    "revision": "dd9bbdbde8645bbaba78",
+    "url": "/robotfriends/static/js/main.0daad29b.chunk.js"
+  },
+  {
+    "revision": "8e2833c355870a1be621",
+    "url": "/robotfriends/static/js/runtime-main.e3489be3.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robotfriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
